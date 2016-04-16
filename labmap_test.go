ae4b0d50cc83ef90ac04e9eb10cf6a84742df971
David Bulkow <david.bulkow@stratus.com>
2016-04-16 17:19:45 -0400

    add testing

---
+++ labmap_test.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestMachine(t *testing.T) {
+	machines = []string{
+		"onemachine",
+		"twomachine",
+		"threemachine",
+		"four",
+	}
+
+	req, _ := http.NewRequest("GET", "", nil)
+	w := httptest.NewRecorder()
+
+	serveMachines(w, req)
+
+	golden, err := ioutil.ReadFile("testdata/machines.golden")
+	if err != nil {
+		t.Fatal("read machine.golden:", err)
+	}
+
+	if bytes.Compare(golden, w.Body.Bytes()) != 0 {
+		t.Error("response != golden")
+	}
+}
+
+func TestCabinets(t *testing.T) {
+	cabinet = make(map[string]*Cabinet)
+
+	readmap("testdata/cabinet.map")
+
+	req, _ := http.NewRequest("GET", "", nil)
+	w := httptest.NewRecorder()
+
+	serveCabinets(w, req)
+
+	golden, err := ioutil.ReadFile("testdata/cabinets.golden")
+	if err != nil {
+		t.Fatal("read cabinets.golden:", err)
+	}
+
+	if bytes.Compare(golden, w.Body.Bytes()) != 0 {
+		t.Error("response != golden")
+	}
+}
