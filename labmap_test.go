3799173e279ee99a4feed8a9c381b6b424f3b5e2
David Bulkow <david.bulkow@stratus.com>
2016-04-17 09:20:13 -0400

    more testing

--- labmap_test.go
+++ labmap_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -34,7 +35,9 @@ func TestMachine(t *testing.T) {
 func TestCabinets(t *testing.T) {
 	cabinet = make(map[string]*Cabinet)
 
-	readmap("testdata/cabinet.map")
+	if err := readmap("testdata/cabinet.map"); err != nil {
+		t.Fatal("readmap error:", err)
+	}
 
 	req, _ := http.NewRequest("GET", "", nil)
 	w := httptest.NewRecorder()
@@ -54,7 +57,9 @@ func TestCabinets(t *testing.T) {
 func TestCabinet(t *testing.T) {
 	cabinet = make(map[string]*Cabinet)
 
-	readmap("testdata/cabinet.map")
+	if err := readmap("testdata/cabinet.map"); err != nil {
+		t.Fatal("readmap error:", err)
+	}
 
 	req, _ := http.NewRequest("GET", "lin302", nil)
 	w := httptest.NewRecorder()
@@ -68,5 +73,43 @@ func TestCabinet(t *testing.T) {
 
 	if bytes.Compare(golden, w.Body.Bytes()) != 0 {
 		t.Error("response != golden")
+	}
+}
+
+func TestCabinetNotFound(t *testing.T) {
+	cabinet = make(map[string]*Cabinet)
+
+	if err := readmap("testdata/cabinet.map"); err != nil {
+		t.Fatal("readmap error:", err)
+	}
+
+	req, _ := http.NewRequest("GET", "linXXX", nil)
+	w := httptest.NewRecorder()
+
+	serveCabinets(w, req)
+
+	var resp struct {
+		Status string `json:"status"`
+		Error  string `json:"error"`
+	}
+
+	if err := json.Unmarshal(w.Body.Bytes(), &resp); err != nil {
+		t.Fatal("unmarshal response:", err)
+	}
+
+	if resp.Status != "Failed" {
+		t.Fatalf("expected 'Failed', got '%s'\n", resp.Status)
+	}
+
+	if resp.Error != "Not Found" {
+		t.Fatalf("expected 'Not Found', got '%s'\n", resp.Error)
+	}
+}
+
+func TestReadmapNoFile(t *testing.T) {
+	cabinet = make(map[string]*Cabinet)
+
+	if err := readmap("testdata/no_file"); err == nil {
+		t.Fatal("expected readmap error, got none")
 	}
 }
